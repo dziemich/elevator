a23ab99939cbd7d5b4e2b8fcacea3b696c9da95f
Michał Dziedzic <michal.dziedzic@schibsted.com>
2019-01-04 18:17:06 +0100

    init

---
+++ backend/_rel/backend_release/lib/backend-0.1.0/src/post_h.erl
@@ -0,0 +1,31 @@
+%% Feel free to use, reuse and abuse the code in this file.
+
+%% @doc POST echo handler.
+-module(post_h).
+
+-export([init/2]).
+
+init(Req0, Opts) ->
+	Method = cowboy_req:method(Req0),
+	HasBody = cowboy_req:has_body(Req0),
+	Req = maybe_echo(Method, HasBody, Req0),
+	{ok, Req, Opts}.
+
+maybe_echo(<<"POST">>, true, Req0) ->
+	{ok, PostVals, Req} = cowboy_req:read_urlencoded_body(Req0),
+	Echo = proplists:get_value(<<"echo">>, PostVals),
+	echo(Echo, Req);
+
+maybe_echo(<<"GET">>, _, Req0) ->
+	echo(get, Req0).
+
+echo(undefined, Req) ->
+	cowboy_req:reply(400, [], <<"Missing echo parameter.">>, Req);
+
+echo(get, Req) ->
+	cowboy_req:reply(200, <<"wspaniale">>, Req);
+
+echo(Echo, Req) ->
+	cowboy_req:reply(200, #{
+		<<"content-type">> => <<"text/plain; charset=utf-8">>
+	}, Echo, Req).
